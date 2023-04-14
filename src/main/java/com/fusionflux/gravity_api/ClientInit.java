65673d39fa5e1bfca284b4a5c36b3c3e049fdcf9
Up <upcraftedlp@gmail.com>
2023-04-14 02:12:37 +0200

    move to quilt entrypoints

--- src/main/java/com/fusionflux/gravity_api/ClientInit.java
+++ src/main/java/com/fusionflux/gravity_api/ClientInit.java
@@ -1,12 +1,13 @@
 package com.fusionflux.gravity_api;
 
 import com.fusionflux.gravity_api.util.GravityChannel;
-import com.fusionflux.gravity_api.util.NetworkUtil;
-import net.fabricmc.api.ClientModInitializer;
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 
 public class ClientInit implements ClientModInitializer {
+
     @Override
-    public void onInitializeClient() {
+    public void onInitializeClient(ModContainer mod) {
         GravityChannel.initClient();
     }
 }
